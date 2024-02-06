2efe22df33b0a695b6e2b698e2ac5f1c143c07b7
stylishsehar <stylishsehar262@gmail.com>
2024-02-06 17:07:56 +0500

    first commit

---
+++ index.js
@@ -0,0 +1,61 @@
+// camel case
+var outDoor = "hello World"
+console.log(outDoor);
+
+// no:4
+var bestMan = "charlie";
+var bestMen = "herry";
+var b = bestMan+bestMen;
+console.log(b);
+
+// no:2
+var s ="salam";
+var f = "walikum assalam";
+console.log(f);
+
+// no:3
+var teamName = "Team Name";
+alert(teamName)
+
+// varaible number no:1
+var s = "caseQty";
+var num = 122
+console.log(s+num);
+
+// no:2
+var caseQty =144
+console.log(caseQty);
+
+// no:3
+var num = 9;
+console.log(9);
+
+// no:4
+var num = 10
+var num = 9
+console.log(num+num);
+
+// no:5
+var merchTotal = 100;
+var shippingCharge = 10;
+var orderTotal = merchTotal + shippingCharge;
+console.log(orderTotal);
+
+// no:6
+ var num = 42
+ var number = 35
+ console.log(num+number);
+
+// varaible name legal and illegal
+
+// no:1
+var productcost = 3.45
+console.log(productcost);
+
+// no:2
+var nameOfBand = "brand"
+console.log(nameOfBand);
+
+
+
+
